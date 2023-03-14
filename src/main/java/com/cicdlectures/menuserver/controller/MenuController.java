98e946859743ed55a364aa37e2d09024bbdaa492
Damien D'ARRAS <damien.darras44@gmail.com>
2023-03-14 13:30:48 +0000

    working delete

--- src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
+++ src/main/java/com/cicdlectures/menuserver/controller/MenuController.java
@@ -8,12 +8,15 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import com.cicdlectures.menuserver.dto.MenuDto;
 import com.cicdlectures.menuserver.service.CreateMenuService;
 import com.cicdlectures.menuserver.service.ListMenuService;
-
 import com.cicdlectures.menuserver.repository.MenuRepository;;
 
 @RestController
@@ -43,10 +46,11 @@ public class MenuController {
     return createMenuService.createMenu(menu);
   }
 
-  @DeleteMapping(path = "/menus/{id}", produces = "application/json", consumes = "application/json")
-  @ResponseStatus(HttpStatus.CREATED)
-  public void deleteMenu(@PathVariable long id) {
-    menuRepository.deleteByID(id);
+  @DeleteMapping(path = "/menus/{id}")
+  public void deleteMenu(@PathVariable("id") Long id) {
+
+    menuRepository.deleteById(id);
+
   }
 
 }
